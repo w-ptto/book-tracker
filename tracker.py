bc4191b70731ec87c5905d37a6c491577b4e7dff
eva <bakaevaeva@gmail.com>
2026-05-24 17:00:05 +1000

    базовые функции

---
+++ tracker.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+FILE_NAME = "books.json"
+
+
+def load_books():
+    if not os.path.exists(FILE_NAME):
+        return []
+
+    with open(FILE_NAME, "r", encoding="utf-8") as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+
+def save_books(books):
+    with open(FILE_NAME, "w", encoding="utf-8") as file:
+        json.dump(books, file, ensure_ascii=False, indent=4)
