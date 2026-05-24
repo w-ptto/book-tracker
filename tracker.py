eb87589bbc32dad11008054b8b5cffb2c64ca75e
eva <bakaevaeva@gmail.com>
2026-05-24 18:14:36 +1000

    добавлена функция удаления книги

--- tracker.py
+++ tracker.py
@@ -18,3 +18,30 @@ def load_books():
 def save_books(books):
     with open(FILE_NAME, "w", encoding="utf-8") as file:
         json.dump(books, file, ensure_ascii=False, indent=4)
+
+def delete_book():
+    books = load_books()
+
+    if not books:
+        print("Список пуст.")
+        return
+
+    show_books()
+
+    try:
+        index = int(input("Введите номер книги для удаления: ")) - 1
+
+        if 0 <= index < len(books):
+            removed = books.pop(index)
+
+            save_books(books)
+
+            print(
+                f"Удалена книга: "
+                f"{removed['author']} — {removed['title']}"
+            )
+        else:
+            print("Неверный номер.")
+
+    except ValueError:
+        print("Введите число.")
