965cffed2b8fb74aa9a52cb9253f5d3997976e8d
eva <bakaevaeva@gmail.com>
2026-05-24 17:26:46 +1000

    добавлена функция просмотра списка книг

--- tracker.py
+++ tracker.py
@@ -18,3 +18,21 @@ def load_books():
 def save_books(books):
     with open(FILE_NAME, "w", encoding="utf-8") as file:
         json.dump(books, file, ensure_ascii=False, indent=4)
+
+def show_books():
+    books = load_books()
+
+    if not books:
+        print("Список книг пуст.")
+        return
+
+    print("\nСписок книг:")
+
+    for index, book in enumerate(books, start=1):
+        print(
+            f"{index}. "
+            f"{book['author']} — "
+            f"{book['title']} | "
+            f"Оценка: {book['rating']} | "
+            f"Дата: {book['date']}"
+        )
